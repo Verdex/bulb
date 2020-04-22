0fca9cc82e1a56e806a7caa6a99d82c473460820
David Inman <davidlevere@gmail.com>
2020-04-21 22:07:38 -0400

    adding sharders

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 
+#[macro_use]
 extern crate glium;
 
 use std::time::Instant;
@@ -13,6 +14,42 @@ use glium::{ Surface
            , glutin::window::WindowBuilder
            , glutin::ContextBuilder
            };
+
+#[derive(Clone, Copy)]
+struct Vertex {
+    position: [f32; 2],
+    texture_coords: [f32; 2],
+}
+
+implement_vertex!(Vertex, position, texture_coords);
+
+const VERTEX_SHADER : &'static str = r#"
+#version 140
+
+in vec2 position;
+in vec2 texture_coords;
+out vec2 v_tex_coords;
+
+uniform mat4 matrix;
+
+void main() {
+    v_tex_coords = texture_coords;
+    gl_Position = matrix * vec4(position, 0.0, 1.0);
+}
+"#;
+
+const FRAGMENT_SHADDER : &'static str = r#"
+#version 140
+
+in vec2 v_tex_coords;
+out vec4 color;
+
+uniform sampler2D tex;
+
+void main() {
+    color = texture(tex, v_tex_coords);
+}
+"#;
 
 fn main() {
     let event_loop = EventLoop::new();
